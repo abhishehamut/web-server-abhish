26182af011059d09aa6dcaa0aa5006248e10ee40
Abhisheha Muthuswamy <abhishehamut@umass.edu>
2026-07-12 07:43:23 -0400

    unit 3 exercises

--- routes/pages.js
+++ routes/pages.js
@@ -10,4 +10,23 @@ router.get('/about', (req, res) => {
   res.send('About page');
 });
 
+router.get('/entries', (req, res) => {
+  const entries = [
+    {
+      title: 'First note',
+      body: 'This is the first note.'
+    },
+    {
+      title: 'Second note',
+      body: 'This is the second note.'
+    },
+    {
+      title: 'Third note',
+      body: 'This is the third note.'
+    }
+  ];
+
+  res.render('entries', { entries });
+});
+
 export default router;
